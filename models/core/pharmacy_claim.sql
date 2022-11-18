d4dc38bfcb50842d678df22cad2701d44aae7532
thuxuan@tuvahealth.com <thuxuan@tuvahealth.com>
2022-11-18 16:33:26 +0000

    snowflake testing complete

---
+++ models/core/pharmacy_claim.sql
@@ -0,0 +1,16 @@
+select 
+ null as claim_id
+, null as claim_line_number
+, null as patient_id
+, null as member_id
+, null as prescribing_provider_npi
+, null as dispensing_provider_npi
+, null as dispensing_date
+, null as ndc_code
+, null as quantity
+, null as days_supply
+, null as refills
+, null as paid_date
+, null as paid_amount
+, null as allowed_amount
+, null as data_source
